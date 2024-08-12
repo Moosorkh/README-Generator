b1aeffdbaddd32261d6c212833fd972aedbfc5fa
fortis <moosorkh@gmail.com>
2024-08-11 17:53:20 -0700

    finished generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-function renderLicenseBadge(license) {
+function renderLicenseBadge(licenseBadge) {
   if (licenseBadge === "MIT") {
     return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
   } else if (licenseBadge === "Apache 2.0") {
@@ -31,13 +31,40 @@ function renderLicenseLink(license) {
 }
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // If there is no license, return an empty string
+  if (!license) {
+    return "";
+  }
+  return `## License
+  This project is licensed under the ${license} license.`;
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ${renderLicenseSection(data.license)}
+  ## Questions
+If you have any questions, please open an issue or contact me directly at ${data.questions.email}. You can find more of my work at [${data.questions.github}](https://github.com/${data.questions.github}).
 `;
 }
 
